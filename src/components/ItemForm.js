f16e49d540b8c7b59c526867792e92f24be2037a
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-05-11 17:31:56 +0300

    controlled components test done

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm ({onItemFormSubmit}) {
+const [name, setName] = useState("");
+const [category, setCategory] = useState("Produce");
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const newItem ={
+    id: uuid(),
+    name,
+    category,
+  };
+ onItemFormSubmit (newItem);
+
+ setName ("");
+ setCategory("Produce");
+ }
   return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
+    <form className="NewItem" onSubmit={handleSubmit}>
+      <label htmlFor="name">
+        Name
+        <input id="name"  type="text" name="name" 
+        value={name}
+        onChange={(e)=> setName(e.target.value)}
+        />
       </label>
 
-      <label>
-        Category:
-        <select name="category">
+      <label htmlFor="category">
+        Category
+        <select  id="category" name="category"
+        value={category}
+        onChange={(e)=>setCategory(e.target.value)}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
